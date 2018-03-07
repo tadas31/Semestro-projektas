b9943f3019ed15b900fb9362e66d88bb28e7a559
karolissto <ltkarolissto@gmail.com>
2018-03-07 17:12:24 +0200

    Minor fixes

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -6,15 +6,17 @@ public class cameraMovement : MonoBehaviour {
 
     public GameObject player;
     private Vector3 offset;
+    private Vector3 startPos;
 
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position; // tarpas tarp kameros ir zaidejo
+        startPos = transform.position;                           // pradine kameros pozicija
+        transform.position = transform.position + new Vector3(1, 0, 0);
     }
 
     // Update is called once per frame
-    void Update () {
-        if(player.transform.position.x > -18)
-            transform.position = new Vector3(player.transform.position.x,1,-10);
-	}
+    void Update () {       
+        transform.position = new Vector3(player.transform.position.x + offset.x, startPos.y, startPos.z); 
+    }
 }
