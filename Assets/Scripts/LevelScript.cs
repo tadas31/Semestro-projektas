042208a04470c50093c49e8ca2655180219dc77e
Tadas Kalvaitis <tadas.k31@gmail.com>
2018-03-16 10:30:51 +0200

    Completed - LEV2, LEV5 Theme - Levels Added pause and back to menu functions

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -7,23 +7,62 @@ using UnityEngine.SceneManagement;
 public class LevelScript : MonoBehaviour {
 
     private Button restart;
-    private string active;
+
+    private Button pause;
+    private GameObject pausePopup;
+
+    private Button home;
+
     // Use this for initialization
     void Start () {
+        Time.timeScale = 1;
+
         restart = GameObject.Find("Restart").GetComponent<Button>();
 
-
+        pause = GameObject.Find("Pause").GetComponent<Button>();
+        pausePopup = GameObject.Find("PausePopup");
+        pausePopup.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
+
         restart.onClick.RemoveAllListeners();
         restart.onClick.AddListener(TaskOnRestartClick);
+
+        pause.onClick.RemoveAllListeners();
+        pause.onClick.AddListener(TaskOnPauseClick);
+
+        if (pausePopup.active == true)
+        {
+            home.onClick.RemoveAllListeners();
+            home.onClick.AddListener(TaskOnHomeClick);
+        }
+        
     }
 
     void TaskOnRestartClick()
     {
-        
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void TaskOnPauseClick()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            pausePopup.SetActive(true);
+            home = GameObject.Find("Home").GetComponent<Button>();
+        }
+        else
+        {
+            pausePopup.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    void TaskOnHomeClick()
+    {
+        SceneManager.LoadScene("LevelSelectionMenu");
     }
 }
