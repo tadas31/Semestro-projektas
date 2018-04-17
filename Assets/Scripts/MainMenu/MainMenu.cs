dc00e155e250e4685d744bfcf1d77763238e0b5b
Tadas Kalvaitis <tadas.k31@gmail.com>
2018-04-17 21:06:11 +0300

    SET4 - Completed Theme - Settings Added possibility to reset game

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -15,8 +15,15 @@ public class MainMenu : MonoBehaviour {
     private Button settings;
     private Button settingsExit;
     private Button Information;
+    private Button shop;
     private GameObject informationPopUp;
     private GameObject settingsPopUp;
+
+    //settings
+    private Button resetGame;
+    private GameObject confirmation;
+    private Button yes;
+    private Button no;
 
 
     void Start()
@@ -28,6 +35,8 @@ public class MainMenu : MonoBehaviour {
 
         Information = GameObject.Find("Information").GetComponent<Button>();
 
+        shop = GameObject.Find("Shop").GetComponent<Button>();
+
         settingsPopUp = GameObject.Find("SettingsPopUp");
         settingsPopUp.SetActive(false);
 
@@ -35,6 +44,10 @@ public class MainMenu : MonoBehaviour {
         informationPopUp.SetActive(false);
 
         candyCount = GameObject.Find("Candy").GetComponentInChildren<Text>();
+
+        confirmation = GameObject.Find("Confirmation");
+        confirmation.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -51,8 +64,20 @@ public class MainMenu : MonoBehaviour {
             int id = Input.GetTouch(0).fingerId;
             if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject(id))
                 TaskOnSettingsExitClick();
+
+            resetGame.onClick.RemoveAllListeners();
+            resetGame.onClick.AddListener(TaskOnResetProgressClick);
+
             settingsExit.onClick.RemoveAllListeners();
             settingsExit.onClick.AddListener(TaskOnSettingsExitClick);
+        }
+
+        if (confirmation.active == true)
+        {
+            yes.onClick.RemoveAllListeners();
+            yes.onClick.AddListener(TaskOnYesClick);
+            no.onClick.RemoveAllListeners();
+            no.onClick.AddListener(TaskOnNoClick);
         }
 
         //open information
@@ -76,10 +101,15 @@ public class MainMenu : MonoBehaviour {
     /// </summary>
     public void TaskOnSettingsClick()
     {
-        levelSelection.gameObject.SetActive(false);
-        settings.gameObject.SetActive(false);
+        levelSelection.interactable = false;
+        settings.interactable = false;
+        Information.interactable = false;
+        shop.interactable = false;
+
         settingsPopUp.active = true;
         settingsExit = GameObject.Find("settingsExit").GetComponent<Button>();
+        resetGame = GameObject.Find("ResetGame").GetComponent<Button>();
+        
     }
 
     /// <summary>
@@ -88,8 +118,11 @@ public class MainMenu : MonoBehaviour {
     public void TaskOnSettingsExitClick()
     {
         settingsPopUp.active = false;
-        levelSelection.gameObject.SetActive(true);
-        settings.gameObject.SetActive(true);
+
+        levelSelection.interactable = true;
+        settings.interactable = true;
+        Information.interactable = true;
+        shop.interactable = true;
     }
 
     /// <summary>
@@ -97,11 +130,10 @@ public class MainMenu : MonoBehaviour {
     /// </summary>
     public void TaskOnInformationClick()
     {
-        if (settingsPopUp.active == true)
-            settingsPopUp.active = false;
+        levelSelection.interactable = false;
+        settings.interactable = false;
+        shop.interactable = false;
 
-        levelSelection.gameObject.SetActive(false);
-        settings.gameObject.SetActive(false);
         informationPopUp.SetActive(true);
     }
 
@@ -110,8 +142,44 @@ public class MainMenu : MonoBehaviour {
     /// </summary>
     public void TaskOnInfromationExitClick()
     {
-        levelSelection.gameObject.SetActive(true);
-        settings.gameObject.SetActive(true);
+        levelSelection.interactable = true;
+        settings.interactable = true;
+        shop.interactable = true;
+
         informationPopUp.SetActive(false);
+    }
+    
+    /// <summary>
+    /// asks if you want to reset your progress
+    /// </summary>
+    private void TaskOnResetProgressClick()
+    {
+        confirmation.SetActive(true);
+        yes = GameObject.Find("Yes").GetComponent<Button>();
+        no = GameObject.Find("No").GetComponent<Button>();
+        settingsPopUp.SetActive(false);
+    }
+
+    /// <summary>
+    /// resets progres
+    /// </summary>
+    private void TaskOnYesClick()
+    {
+        SaveManager.Instance.ResetSave();
+        SaveManager.Instance.Load();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// goes back to settings
+    /// </summary>
+    private void TaskOnNoClick()
+    {
+        levelSelection.interactable = true;
+        settings.interactable = true;
+        Information.interactable = true;
+        shop.interactable = true;
+
+        confirmation.SetActive(false);
     }
 }
