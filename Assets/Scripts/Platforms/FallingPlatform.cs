154ce4ebeefdc40366007401ff60d4d9c911ac2a
Tadas Kalvaitis <tadas.k31@gmail.com>
2018-07-04 17:54:57 +0300

    LEV17 - In progress Theme - Levels

--- Assets/Scripts/Platforms/FallingPlatform.cs
+++ Assets/Scripts/Platforms/FallingPlatform.cs
@@ -22,7 +22,8 @@ public class FallingPlatform : MonoBehaviour {
             spikes.constraints = RigidbodyConstraints2D.None;
         }
         if (Input.GetMouseButtonDown(0) && cameraMovement.moveCamera)
-            RemoveSpike();   
+            RemoveSpike();
+               
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -35,6 +36,9 @@ public class FallingPlatform : MonoBehaviour {
         Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
         if (hit && hit.collider.tag == "FallingSpikes")
-            hit.collider.gameObject.SetActive(false);       
+        {
+            hit.collider.gameObject.SetActive(false);
+            TutorialScript.wasFallingSpikesRemoved = true;
+        }
     }
 }
--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -8,16 +8,29 @@ public class TutorialScript : MonoBehaviour {
     //kad galeciau stabdyt platformas pakeist level skripte esanti boola canstopplatform i true
 
     public static bool wasCameraMoved;              //true if camera was moved else false
+    public static bool wasFallingSpikesRemoved;     //true if spikes was removed else false
     private Text cameraMovementText;                //camera movement text
     private Text fallingSpikesText;                 //falling spikes text
+    private Text movingPlatformsText;               //moving platforms text
+    private Text doneText;                          //done button text
 
 
     // Use this for initialization
     void Start () {
-        cameraMovementText = GameObject.Find("CameraMovementText").GetComponent<Text>();
         wasCameraMoved = false;
+        wasFallingSpikesRemoved = false;
+
+        //gets all tutorial text fields
+        cameraMovementText = GameObject.Find("CameraMovementText").GetComponent<Text>();
         fallingSpikesText = GameObject.Find("FallingSpikesText").GetComponent<Text>();
+        movingPlatformsText = GameObject.Find("MovingPlatformsText").GetComponent<Text>();
+        doneText = GameObject.Find("DoneText").GetComponent<Text>();
+
+        //makes tutorial text fields inactive
         fallingSpikesText.gameObject.SetActive(false);
+        movingPlatformsText.gameObject.SetActive(false);
+        doneText.gameObject.SetActive(false);
+
     }
 	
 	// Update is called once per frame
@@ -28,6 +41,20 @@ public class TutorialScript : MonoBehaviour {
         }
         else if (!cameraMovementText.IsActive() && fallingSpikesText.IsActive())
         {
+            if (wasFallingSpikesRemoved)
+            {
+                fallingSpikesText.gameObject.SetActive(false);
+                LevelScript.canStopPlatforms = true;
+                movingPlatformsText.gameObject.SetActive(true);
+            }
+        }
+        else if (!fallingSpikesText.IsActive() && movingPlatformsText.IsActive())
+        {
+            if (PlatformScript.stoppedCount == PlatformScript.stoppableCount)
+            {
+                movingPlatformsText.gameObject.SetActive(false);
+                doneText.gameObject.SetActive(true);
+            }
         }
     }
 
