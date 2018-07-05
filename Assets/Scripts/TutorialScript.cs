6831c1b16bae19922d977cb0d288579c4cfe3db2
Tadas Kalvaitis <tadas.k31@gmail.com>
2018-07-05 18:21:23 +0300

    LEV17 - In progress Theme - Levels

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -3,65 +3,120 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TutorialScript : MonoBehaviour {
+public class TutorialScript : MonoBehaviour
+{
 
-    //kad galeciau stabdyt platformas pakeist level skripte esanti boola canstopplatform i true
+    private Vector3 characterStartingPosition = new Vector3(-3.891f, 1.03443f, 0);  //character starting position
+    private float timer;
 
     public static bool wasCameraMoved;              //true if camera was moved else false
     public static bool wasFallingSpikesRemoved;     //true if spikes was removed else false
+    private bool jump;                              //true if jumped else false
     private Text cameraMovementText;                //camera movement text
     private Text fallingSpikesText;                 //falling spikes text
     private Text movingPlatformsText;               //moving platforms text
     private Text doneText;                          //done button text
+    private Text characterMovementText;             //character movement text
+    private Text enemyText;                         //enemy text
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        timer = 0;
+
         wasCameraMoved = false;
         wasFallingSpikesRemoved = false;
+        jump = false;
 
         //gets all tutorial text fields
         cameraMovementText = GameObject.Find("CameraMovementText").GetComponent<Text>();
         fallingSpikesText = GameObject.Find("FallingSpikesText").GetComponent<Text>();
         movingPlatformsText = GameObject.Find("MovingPlatformsText").GetComponent<Text>();
         doneText = GameObject.Find("DoneText").GetComponent<Text>();
+        characterMovementText = GameObject.Find("CharacterMovementText").GetComponent<Text>();
+        enemyText = GameObject.Find("EnemyText").GetComponent<Text>();
 
         //makes tutorial text fields inactive
         fallingSpikesText.gameObject.SetActive(false);
         movingPlatformsText.gameObject.SetActive(false);
         doneText.gameObject.SetActive(false);
+        characterMovementText.gameObject.SetActive(false);
+        enemyText.gameObject.SetActive(false);
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (wasCameraMoved && cameraMovementText.IsActive())
         {
-            StartCoroutine(changeCameraMovemenText());
+            timer += Time.deltaTime;
+            if (timer >= 1)
+            {
+                cameraMovementText.gameObject.SetActive(false);
+                fallingSpikesText.gameObject.SetActive(true);
+            }
+            if (wasFallingSpikesRemoved)
+            {
+                cameraMovementText.gameObject.SetActive(false);
+                movingPlatformsText.gameObject.SetActive(true);
+            }
         }
+
         else if (!cameraMovementText.IsActive() && fallingSpikesText.IsActive())
         {
             if (wasFallingSpikesRemoved)
             {
-                fallingSpikesText.gameObject.SetActive(false);
-                LevelScript.canStopPlatforms = true;
-                movingPlatformsText.gameObject.SetActive(true);
+               
+                StartCoroutine(DelayHalfS(fallingSpikesText, movingPlatformsText));
             }
         }
+
         else if (!fallingSpikesText.IsActive() && movingPlatformsText.IsActive())
         {
+            LevelScript.canStopPlatforms = true;
             if (PlatformScript.stoppedCount == PlatformScript.stoppableCount)
             {
                 movingPlatformsText.gameObject.SetActive(false);
                 doneText.gameObject.SetActive(true);
             }
         }
+
+        else if (!movingPlatformsText.IsActive() && doneText.IsActive())
+        {
+            if (!cameraMovement.moveCamera)
+            {
+                doneText.gameObject.SetActive(false);
+                characterMovementText.gameObject.SetActive(true);
+            }
+        }
+
+        else if (!doneText.IsActive() && characterMovementText.IsActive())
+        {
+            if (CharacterScript.rdbd.position.y > characterStartingPosition.y)
+                jump = true;
+
+            if (CharacterScript.rdbd.position.x != characterStartingPosition.x && jump)
+            {
+                StartCoroutine(DelayOneS(characterMovementText, enemyText));
+               
+            }
+        }
     }
 
-    IEnumerator changeCameraMovemenText()
+    IEnumerator DelayOneS(Text disable, Text enable)
     {
         yield return new WaitForSeconds(1.0f);
-        cameraMovementText.gameObject.SetActive(false);
-        fallingSpikesText.gameObject.SetActive(true);
+        disable.gameObject.SetActive(false);
+        enable.gameObject.SetActive(true);
+        
+    }
+
+    IEnumerator DelayHalfS(Text disable, Text enable)
+    {
+        yield return new WaitForSeconds(0.5f);
+        disable.gameObject.SetActive(false);
+        enable.gameObject.SetActive(true);
     }
 }
